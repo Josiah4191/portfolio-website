dbf94caa00650b7fc99cff38b280df413f222036
Josiah Stoltzfus <Josiah4191@gmail.com>
2026-05-22 13:50:46 -0500

    Update another project url

--- src/features/portfolio/data/Project-data.js
+++ src/features/portfolio/data/Project-data.js
@@ -64,7 +64,7 @@ export const PROJECTS = [
             links: [
                 {
                     label: "GitHub",
-                    url: "https://github.com/Josiah4191/portfolio-website",
+                    url: "https://github.com/Josiah4191/portfolio-website-original",
                 }
             ],
             lessonsLearned: [
