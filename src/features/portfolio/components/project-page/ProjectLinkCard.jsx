a79dc1aba98bc8ee558212a1c9dac570b110dfc3
Josiah Stoltzfus <Josiah4191@gmail.com>
2026-05-22 14:22:38 -0500

    Update target attribute url

--- src/features/portfolio/components/project-page/ProjectLinkCard.jsx
+++ src/features/portfolio/components/project-page/ProjectLinkCard.jsx
@@ -2,6 +2,6 @@ import './ProjectPage.css';
 
 export default function ProjectLinkCard({url, label}) {
     return (
-        <a target={url === 'http://localhost:5173/squirrels' ? "_self" : "_blank"} href={url} className="project-link-card">{label} <span className="project-link-arrow">→</span></a>
+        <a target={"_blank"} href={url} className="project-link-card">{label} <span className="project-link-arrow">→</span></a>
     )
 }
